7683a4bb0d5bd4f4b0c5a0e2c63255f9d5060f1c
Darkzarich <darkzarichv2@gmail.com>
2026-03-29 01:34:35 +0300

    rate limiting with ticker

---
+++ goroutines/rate_limiting/rate_limiting.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func rateLimitedWorker(jobs chan int, rate time.Duration) {
+	limiter := time.NewTicker(rate)
+	defer limiter.Stop()
+
+	for job := range jobs {
+		<-limiter.C // wait for next tick
+		fmt.Printf("Processing job %d at %s\n", job, time.Now().Format(time.RFC3339))
+	}
+}
+
+func main() {
+	jobs := make(chan int, 10)
+
+	// send 10 jobs
+	for i := 1; i <= 10; i++ {
+		jobs <- i
+	}
+	close(jobs)
+
+	// process at most 2 jobs per second
+	rateLimitedWorker(jobs, 500*time.Millisecond)
+}
