7e9154306df84efd3c677dcb1e6b35f0bc26383e
Darkzarich <darkzarichv2@gmail.com>
2026-05-05 22:14:02 +0300

    better random numbers generator

---
+++ interface/better-random-numbers-generator/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"math/rand"
+	"time"
+)
+
+type generator struct {
+	// Actually rand.Rand implements io.Reader, but for the sake of example we implement it separately.
+	rnd rand.Source
+}
+
+// Do note that we do not let use generator struct directly, but instead we create a New function that returns a io.Reader.
+// the struct is different type but it implements the same interface
+func New(seed int64) io.Reader {
+	return &generator{
+		rnd: rand.NewSource(seed),
+	}
+}
+
+// Read is the only method we need to implement.
+func (g *generator) Read(bytes []byte) (n int, err error) {
+	cyclesCount := len(bytes) / 8 // 8 bytes per 64 bit integer
+
+	for i := 0; i < cyclesCount; i++ {
+		randInt := g.rnd.Int63()
+		binary.LittleEndian.PutUint64(bytes[i*8:], uint64(randInt))
+	}
+
+	return len(bytes), nil
+}
+
+func main() {
+	generator := New(time.Now().UnixNano()) // seed is the current time in nanoseconds
+
+	buf := make([]byte, 16)
+
+	for i := 0; i < 5; i++ {
+		n, _ := generator.Read(buf) // the only method available on interface
+		fmt.Printf("Generate bytes: %v size(%d)\n", buf, n)
+	}
+
+	// Using existing in std library rand.Rand
+	generator2 := rand.New(rand.NewSource(time.Now().UnixNano())) // seed is the current time in nanoseconds
+
+	for i := 0; i < 5; i++ {
+		n, _ := generator2.Read(buf) // the only method available on interface
+		fmt.Printf("Generate bytes [2]: %v size(%d)\n", buf, n)
+	}
+}
