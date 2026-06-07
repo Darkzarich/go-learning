329a16afee23f7a8c9171601c5757e5a2d5bdf71
Darkzarich <darkzarichv2@gmail.com>
2026-06-07 19:50:40 +0300

    go testing add table-driven tests generated with gotests

--- testing/sum/sum/sum_test.go
+++ testing/sum/sum/sum_test.go
@@ -33,3 +33,67 @@ func TestSumZero(t *testing.T) {
 		t.Error("both args zero - expected error not be nil")
 	}
 }
+
+// Table-driven tests generated with gotests
+func TestSum(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		// Cases
+		{
+			name: "positive",
+			args: args{
+				a: 1,
+				b: 2,
+			},
+			want:    3,
+			wantErr: false,
+		},
+		{
+			name: "negative",
+			args: args{
+				a: -1,
+				b: 2,
+			},
+			want:    0,
+			wantErr: true,
+		},
+		{
+			name: "negative all",
+			args: args{
+				a: -1,
+				b: -3,
+			},
+			want:    0,
+			wantErr: true,
+		},
+		{
+			name: "zero",
+			args: args{
+				a: 0,
+				b: 0,
+			},
+			want:    0,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Sum(tt.args.a, tt.args.b)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Sum() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Sum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
